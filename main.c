0e030ffc8edcac00049ba0f479e232ddc44f56df
xk <permission@hnu.edu.cn>
2018-11-07 22:19:09 +0800

    new main

--- main.c
+++ main.c
@@ -1,57 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pcap.h>
 #include <pkt_hdr.h>
+#include <load_trace.h>
 #include <limits.h>//USHRT_MAX
 #include <string.h>//bzero
+#include <parse_pp_cmd.h>
+#include <config.h>
+#include <pkts_to_pcap.h>
 
-#define NB_PKT 1024
-#define LINKTYPE_ETHERNET 1
-#define TCPDUMP_MAGIC	0xa1b2c3d4
+#define NB_PKT 65535
 #define PKT_SIZE 64
 
-void fill_pp_hdr(struct pp_hdr * _pp_hdr, uint32_t _action, uint32_t _id)
+struct pp_hdr pkts[NB_PKT];
+struct pp_config cur_conf;
+
+int main(int argc, char ** argv)
 {
-	for(uint32_t i=0; i < 32; i++){
-		_pp_hdr->payload[i] = i;
+
+	int ret;
+
+	//init conif	
+	init_config(&cur_conf);
+
+	//parse args
+	parse_pp_cmd(argc, argv, &cur_conf);
+
+	//do load default trace
+	if(cur_conf.is_default == 1){
+		ret = load_default_trace(pkts, NB_PKT);
+		printf("Default trace : load %d lines\n", ret);
 	}
+	//load trace from file
+	else{
+		ret = load_pp_trace(cur_conf.file_path, pkts, NB_PKT);
+		printf("PP trace : load %d lines\n", ret);
+	}	
 
-	_pp_hdr->action = _action;
-	_pp_hdr->id = _id;
-}
-
-int main() {
-  pcap_t *pd;
-  pcap_dumper_t *pdumper;
-
-  pd = pcap_open_dead(DLT_EN10MB, 65535);
-  pdumper = pcap_dump_open(pd, "pp.pcap");
-
-  struct pcap_pkthdr packet_header;
-  struct timeval ts;
-
-  packet_header.ts = ts;
-  packet_header.caplen = 64;
-  packet_header.len = 64;
-
-  struct pp_hdr cur_pp_hdr[NB_PKT];
-	int i;
-
-	for(i = NB_PKT - 1 ; i>=0; --i){
-		//zero dst and src mac
-		bzero(&cur_pp_hdr[i].eth.d_addr, sizeof(cur_pp_hdr[i].eth.d_addr));			
-		bzero(&cur_pp_hdr[i].eth.s_addr, sizeof(cur_pp_hdr[i].eth.s_addr));			
-		
-		cur_pp_hdr[i].eth.ether_type = PKT_SIZE - sizeof(struct ether_hdr);
-		fill_pp_hdr(&cur_pp_hdr[i], (uint32_t)i, (uint32_t)i);
-	}
-	
-	for(i = NB_PKT - 1 ; i>=0; --i){
-			pcap_dump((u_char*)pdumper, &packet_header, (const u_char*)&cur_pp_hdr[i]);
-	}
-
-  pcap_close(pd);
-  pcap_dump_close(pdumper);
+	//convert to pcap
+	do_convert(pkts, ret);
 
 	return 0;
 }
